3fc56f5042ee7fc68dabfc57715de8be1fa6a11a
HuDery <duyhl0211@gmail.com>
2024-03-14 21:23:01 +0700

    Add Required

--- Repositories/DTOs/CreateEmployeeDTO.cs
+++ Repositories/DTOs/CreateEmployeeDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,11 @@ namespace Repositories.DTOs
 {
     public class CreateEmployeeDTO
     {
+        [Required]
         public string EmployeeName { get; set; } = null!;
+        [Required]
         public DateTime BirthDay { get; set; }
+        [Required]
         public int RoleId { get; set; }
     }
 }
